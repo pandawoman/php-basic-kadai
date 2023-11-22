daa1db96b70d2f0a8f019f31934203505d5c8bd2
pandawoman <149503801+pandawoman@users.noreply.github.com>
2023-11-23 00:09:51 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset = "UTF-8">
+  <title>kadai_005</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160,];
+
+    print_r($vegetable_data);
+
+    ?>
+   </P>
+</body>
+
+</html>
+  
